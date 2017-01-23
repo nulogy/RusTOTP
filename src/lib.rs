c7c6a2f76842faea83f3c50b4bb4eb1e21b78761
Ryan De Villa <ryand@nulogy.com>
2017-01-23 12:36:09 -0500

    Complete implementation of HOTP.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ use crypto::mac::Mac;
 use crypto::hmac::Hmac;
 use crypto::sha1::Sha1;
 
+use std::mem;
+
 pub fn hmac_sha1(k : &[u8], c : &[u8]) -> Vec<u8> {
     let mut hmac = Hmac::new(Sha1::new(), k);
     for i in 0..c.len() {
@@ -11,4 +13,26 @@ pub fn hmac_sha1(k : &[u8], c : &[u8]) -> Vec<u8> {
     }
     let result = hmac.result();
     result.code().to_vec()
+}
+
+pub fn offset(b : &[u8]) -> usize {
+    (b.last().unwrap() & 0xf) as usize
+}
+
+pub fn sbits(bits: &[u8]) -> u32 {
+    let offset = offset(bits);
+    //unsafe { mem::transmute::<[u8; 4], u32>([bits[offset], bits[offset+1], bits[offset+2], bits[offset+3]]) }
+    unsafe { mem::transmute::<[u8; 4], u32>(
+            [
+            bits[offset+3],
+            bits[offset+2],
+            bits[offset+1],
+            bits[offset] & 0x7f
+            ]
+            )
+    }
+}
+
+pub fn hotp(snum : u32) -> u32 {
+    snum % 10u32.pow(6)
 }
--- tests/test_main.rs
+++ tests/test_main.rs
@@ -4,15 +4,49 @@ fn format_hex(s: &[u8]) -> String {
     let mut human_representation: Vec<String> = Vec::new();
     
     for c in s.iter() {
-        human_representation.push(format!("Ox{:x}", c))
+        human_representation.push(format!("{:02x}", c))
     }
-    format!("{}", human_representation.join(", "))
+    format!("0x{}", human_representation.join(""))
 }
 
 #[test]
 fn it_works() {
     let counter : &[u8] = &[ 0x00; 8 ];
-    let result = rfc6238::hmac_sha1(&[0x0b; 20], "Hi There".as_bytes());
-    println!("{}", format_hex(result.as_slice()));
+    println!("HMAC-SHA-1:");
+    println!("================================================================================");
+    let example : &[u8] = &[
+        0x1f,
+        0x86,
+        0x98,
+        0x69,
+        0x0e,
+        0x02,
+        0xca,
+        0x16,
+        0x61,
+        0x85,
+        0x50,
+        0xef,
+        0x7f,
+        0x19,
+        0xda,
+        0x8e,
+        0x94,
+        0x5b,
+        0x55,
+        0x5a
+    ];
+
+    println!("HMAC SHA-1: {}", format_hex(example));
+    println!("================================================================================");
+
+    println!("Offset index: {}", rfc6238::offset(example));
+    println!("================================================================================");
+
+    println!("P: {:x}", rfc6238::sbits(example));
+    println!("================================================================================");
+
+    println!("HOTP Value: {}", rfc6238::hotp(rfc6238::sbits(example)));
+    println!("================================================================================");
     assert!(false);
 }
