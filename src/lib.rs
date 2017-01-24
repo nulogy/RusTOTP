8b9f7d014a889765dd034aab6261fa7902ff3986
Justin Fitzsimmons <justinf@nulogy.com>
2017-01-24 11:10:12 -0500

    Resolves  #1, resolves #2, resolves #3
    hotp now returns a HotpOutput, a struct containing the code and the final string length. HotpOutput implements fmt::Display. Padding of the string now uses variable length std::fmt string instead of a hand-rolled padding function.
    
    Also cleans up a lot of the integer types and thus removes a large number of type coercions.

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use crypto::mac::Mac;
 use crypto::hmac::Hmac;
 use crypto::sha1::Sha1;
 
-use std::mem;
+use std::{mem, fmt};
 
 fn hmac_sha1(key: &[u8], message: &[u8]) -> Vec<u8> {
     let mut hmac = Hmac::new(Sha1::new(), key);
@@ -31,13 +31,24 @@ fn sbits(bits: &[u8]) -> u32 {
     }
 }
 
-pub fn hotp(desired_code_length: u8, key: &[u8], counter: &[u8]) -> u32 {
-    let hmac_value = hmac_sha1(key, counter);
-    let dbc = sbits(hmac_value.as_slice());
-    dbc % 10u32.pow(desired_code_length as u32)
+pub struct HotpOutput {
+    pub code: u32,
+    pub length: usize,
 }
 
-fn to_bytes(x: u64) -> [u8; 8] {
+impl fmt::Display for HotpOutput {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:0length$}", self.code, length = self.length)
+    }
+}
+
+pub fn hotp(desired_code_length: usize, key: &[u8], counter: &[u8]) -> HotpOutput {
+    let hmac_value = hmac_sha1(key, counter);
+    let dbc = sbits(hmac_value.as_slice());
+    HotpOutput{ code: dbc % 10u32.pow(desired_code_length as u32), length: desired_code_length }
+}
+
+fn to_bytes(x: i64) -> [u8; 8] {
     let mut temp = [0u8; 8];
     for byte_index in 0..8 {
         let shift_amount: usize = 8 * (7 - byte_index);
@@ -46,22 +57,7 @@ fn to_bytes(x: u64) -> [u8; 8] {
     temp
 }
 
-pub fn totp(desired_code_length: u8, timestep: u64, key: &[u8], time: u64) -> u32 {
-    let t0 = 0;
-
-    let t = (time - t0) / timestep;
-
+pub fn totp(desired_code_length: usize, timestep: i64, key: &[u8], time: i64) -> HotpOutput {
+    let t = time / timestep;
     hotp(desired_code_length, key, &to_bytes(t))
-}
-
-pub fn format_code(code: u32, width: usize) -> String {
-    let mut output = String::with_capacity(width);
-    let string_code = format!("{}", code);
-    let padding_size = width - string_code.len();
-
-    for _ in 0..padding_size {
-        output.push('0');
-    }
-    output.push_str(string_code.as_str());
-    output
 }
--- tests/test_main.rs
+++ tests/test_main.rs
@@ -1,6 +1,6 @@
 extern crate rfc6238;
 
-use rfc6238::{hotp, totp, format_code};
+use rfc6238::{hotp, totp};
 
 pub fn format_hex(s: &[u8]) -> String {
     let mut human_representation: Vec<String> = Vec::new();
@@ -18,7 +18,7 @@ fn hotp_conforms_to_example_1_given_in_rfc6238() {
     let mut c_buffer = [0; 8];
     c_buffer[7] = 1;
     let totp_value = hotp(8, key, &c_buffer);
-    assert_eq!(94287082, totp_value);
+    assert_eq!(94287082, totp_value.code);
 }
 
 #[test]
@@ -35,7 +35,7 @@ fn hotp_conforms_to_example_4_given_in_rfc6238() {
         0xec
     ];
     let totp_value = hotp(8, key, &c_buffer);
-    assert_eq!(07081804, totp_value);
+    assert_eq!(07081804, totp_value.code);
 }
 
 #[test]
@@ -52,7 +52,7 @@ fn hotp_conforms_to_example_7_given_in_rfc6238() {
         0xed
     ];
     let totp_value = hotp(8, key, &c_buffer);
-    assert_eq!(14050471, totp_value);
+    assert_eq!(14050471, totp_value.code);
 }
 
 #[test]
@@ -60,19 +60,5 @@ fn totp_conforms_to_example_1_given_in_rfc6238() {
     let key = "12345678901234567890".as_bytes();
     let current_time = 59;
     let totp_value = totp(8, 30, key, current_time);
-    assert_eq!(94287082, totp_value);
-}
-
-#[test]
-fn pads_a_code() {
-    let code = 12345;
-    let width = 6;
-    assert_eq!("012345", format_code(code, width));
-}
-
-#[test]
-fn does_not_pad_a_full_code() {
-    let code = 654321;
-    let width = 6;
-    assert_eq!("654321", format_code(code, width));
+    assert_eq!(94287082, totp_value.code);
 }
